06ae10b75acf9ddd50e3dcc6ce291b521400db7b
Rahul <rahulna1996@gmail.com>
2024-01-19 22:45:03 +0530

    Initial commit

---
+++ portfolio-website/src/App.jsx
@@ -0,0 +1,25 @@
+import {} from "react";
+import "./App.css";
+import Navbar from "./component/Navbar";
+import About from "./component/About.jsx";
+import Skills from "./component/Skills";
+import Projects from "./component/Projects";
+import Testimonials from "./component/Testimonials";
+import Contact from "./component/Contact";
+
+function App() {
+  console.log("App");
+  return (
+    <>
+      <h1>My Portfolio website</h1>
+      <Navbar />
+      <About />
+      <Skills />
+      <Projects />
+      <Testimonials />
+      <Contact />
+    </>
+  );
+}
+
+export default App;
