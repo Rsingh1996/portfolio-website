7f80e891aad7b68884114b2b7e0c1b2563d8c680
Rahul <rahulna1996@gmail.com>
2024-01-23 23:07:36 +0530

    completed navbar and added home component

--- portfolio-website/src/component/Home.jsx
+++ portfolio-website/src/component/Home.jsx
@@ -11,13 +11,25 @@ import { ClassNames } from "@emotion/react";
 const Home = () => {
   return (
     <div className=" w-full h-screen bg-[#0a192f]">
-      {/* <h1 className="text-2xl font-bold">My Portfolio website</h1> */}
-      {/* <Navbar /> */}
-      {/* <About />
-      <Skills />
-      <Projects />
-      <Testimonials />
-      <Contact /> */}
+      {/* container */}
+      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+        <p1 className="text-yellow-600">My name is</p1>
+        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
+          Rahul Singh
+        </h1>
+        <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
+          I'm a React Developer
+        </h2>
+        <p1 className="text-[#8892b0] py-4 max-w-[700px]">lorum epsom</p1>
+        <div>
+          <button className="text-white group border-2 px-6 py-3 mx-2 flex items-center hover:bg-yellow-600 hover:border-yellow-600">
+            View Work{" "}
+            <span className="group-hover:rotate-90 duration-300">
+              <HiArrowNarrowRight className="ml-3" />
+            </span>
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
--- portfolio-website/src/component/Navbar.jsx
+++ portfolio-website/src/component/Navbar.jsx
@@ -38,7 +38,7 @@ const Navbar = () => {
         <li className="py-6 text-4xl">Contact</li>
       </ul>
       {/* social icons*/}
-      <div className="  flex  fixed flex-col top-[35%] left-0">
+      <div className="hidden lg:flex    fixed flex-col top-[35%] left-0">
         <ul>
           <li className=" flex justify-between items-center w-[160px] h-[60px] ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600">
             <a
