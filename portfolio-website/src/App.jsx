67ebf38b858d399f1fcb20dba6ad1757e0e7889e
Rahul <rahulna1996@gmail.com>
2024-01-25 21:42:39 +0530

    Styling Project component

--- portfolio-website/src/App.jsx
+++ portfolio-website/src/App.jsx
@@ -14,6 +14,7 @@ function App() {
       <Home />
       <About />
       <Skills />
+      <Projects />
       {/* <Router>
         <Routes>
           <Route path="/" element={<Home />} />
--- portfolio-website/src/component/Projects.jsx
+++ portfolio-website/src/component/Projects.jsx
@@ -1,10 +1,36 @@
 import React from "react";
+import makeyourtrip from "../assets/makeyourtrip.png";
 
 const Projects = () => {
   return (
-    <>
-      <h1>Projects</h1>
-    </>
+    <div className="w-full md:h-screen text-gray-300 bg-[#0a192f]">
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-yellow-600">
+            Projects
+          </p>
+          <p className="py-4">Checkout some of my recent Projects</p>
+        </div>
+        <div
+          style={{ backgroundImage: `url(${makeyourtrip})` }}
+          className="grid sm:grid-cols-2 md:grid-cols-3: gap-4 "
+        >
+          <div className="shadow-lg shadow-[#040c26] group container rounded-md flex justify-center items-center mx-auto content-div">
+            <div>
+              <span></span>
+              <div>
+                <a href="/">
+                  <button></button>
+                </a>
+                <a href="/">
+                  <button></button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
